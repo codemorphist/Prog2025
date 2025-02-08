64602f4fd137068751b2090f9607222a3aaf6700
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-02-08 10:34:29 +0200

    Completed and tested Task 25.6

--- HW2/packet_stream.py
+++ HW2/packet_stream.py
@@ -61,42 +61,38 @@ def get_packets(data: bytes) -> Iterator[Packet]:
             yield packet
 
 
+def recvall(socket: socket.socket, size: int) -> bytes:
+    data = b""
+
+    while len(data) < size:
+        part = socket.recv(size - len(data))
+        if not part:
+            break
+        data += part
+
+    return data
+
+
 def send_data(socket: socket.socket, data: bytes):
     # send header
     header = pack_header(get_header(data))  
     socket.sendall(header)
-    print(f"HEADER: {len(header)}")
 
     # send packets
     for packet in get_packets(data):
         send_packet = pack_packet(packet) 
         socket.sendall(send_packet)
-        print(f"PACKET: {len(send_packet)}")
 
 
 def recv_data(socket: socket.socket) -> bytes:
     # recv header
-    recv_header = b""
-    while len(recv_header) < HEADER_SIZE:
-        part = socket.recv(HEADER_SIZE - len(recv_header))
-        if not part:
-            break
-        recv_header += part
+    recv_header = recvall(socket, HEADER_SIZE)
     header = unpack_header(recv_header)
-    print(f"HEADER: {len(recv_header)}")
-
 
     # recv packets
     data = bytes()
     for _ in range(header.parts):
-        recv_packet = b""
-        while len(recv_packet) < PACKET_SIZE:
-            part = socket.recv(PACKET_SIZE - len(recv_packet))
-            if not part:
-                break
-            recv_packet += part
-        
-        print(f"PACKET: {len(recv_packet)}")
+        recv_packet = recvall(socket, PACKET_SIZE)
         packet = unpack_packet(recv_packet)
         data += packet.data
 
@@ -111,26 +107,21 @@ def send_file(socket: socket.socket,
               filepath: str, savepath: str):
     send_data(socket, encode(f"SAVE_TO {savepath}"))
 
-    sended_size = 0
     with open(filepath, "rb") as f:
         while data := f.read(1024):
             send_data(socket, data)
-            sended_size += len(data)
     send_data(socket, b"EOF")
-    print(sended_size)
 
 
-def recv_file(socket: socket.socket):
+def recv_file(socket: socket.socket) -> str:
     data = recv_data(socket).decode().split()
     savepath = data[1]
 
-    recived_size = 0
     with open(savepath, "wb") as f:
         while True:
             data = recv_data(socket)
             if data == b"EOF":
                 break
-            recived_size += len(data)
             f.write(data)
-    print(recived_size)
+    return savepath
 
