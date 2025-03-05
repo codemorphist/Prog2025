359a3bf38568bdfee618b90f893aa32c884c2228
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-03-05 09:04:15 +0200

    Updated task

--- CW4/t26_1.py
+++ CW4/t26_1.py
@@ -1,6 +1,7 @@
 import re
 import requests
-
+import openpyxl
+from datetime import datetime
 
 SYNOPTIC_URL = "https://sinoptik.ua"
 SYNOPTIC_CITY = "Львов"
@@ -18,8 +19,25 @@ def regex_parse():
     cur_temp = re.findall("<p class=\"_6fYCPKSx\">(.*?)</p>", page_html)[0]
     min_temp, max_temp = temp_div[:2]
 
-    print(cur_temp, min_temp, max_temp)
+    return cur_temp, min_temp, max_temp
+
+
+def update_data(filename: str = "temp_stat.xlsx"):
+
+    wb = openpyxl.Workbook()
+    
+    ws = wb.active
+
+    ws["A1"] = "Дата"
+    ws["A2"]
+
+
+    cur_data = datetime.today().strftime('%Y-%m-%d')
+    cur_temp, min_temp, max_temp = regex_parse()
+    ws.append([])
+
+    wb.save(filename)
 
 
 if __name__ == "__main__":
-    regex_parse()
+    update_data()
