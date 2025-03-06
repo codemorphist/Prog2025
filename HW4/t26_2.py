d4949ecfa5e91e727942b4d5dcc076e491433a10
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-03-06 10:39:47 +0200

    Updated T26.2
    - Added weather data
    - Added cell styles

--- HW4/t26_2.py
+++ HW4/t26_2.py
@@ -2,6 +2,7 @@ import os
 import re
 import requests
 import openpyxl
+from openpyxl.styles import PatternFill, Font, Border, Side, Color
 from datetime import datetime
 
 
@@ -10,6 +11,12 @@ def get_html(city: str) -> str:
     url = f"{SYNOPTIC_URL}/погода-{city.lower()}" 
     responce = requests.get(url) 
     return responce.text
+
+
+def get_cur_weather(html: str) -> str:
+    WEATHER_REGEX = r"<div class=\"bSOXy2ra N5FMbQtj\" aria-label=\"(.*?)\"></div>"
+    weather = re.findall(WEATHER_REGEX, html)
+    return weather[0] if weather else ""
 
 
 def get_cur_temp(html: str) -> str:
@@ -33,15 +40,48 @@ def get_min_max_temp(html: str) -> list[str]:
     MIN_MAX_TEMP_REGEX = r"<div class=\"\+Ncy59Ya\">.*?<p>\+?(\-?[0-9]*).*?<\/p>"
     temps = re.findall(MIN_MAX_TEMP_REGEX, html)
 
-    return resize_list(temps, 6 * 2, "")
+    return resize_list(temps, 5 * 2, "")
 
 
-def parse_temp(city: str) -> list[str]:
+def parse_data(city: str) -> list[str]:
     html = get_html(city)
     cur_temp = get_cur_temp(html)
+    cur_weather = get_cur_weather(html)
     five_days_temp  = get_min_max_temp(html)
 
-    return [cur_temp, *five_days_temp]
+    return [cur_temp, cur_weather, *five_days_temp]
+
+
+def config_ws(ws):
+    ws["A1"] = "Дата"
+    ws["B1"] = "Поточна температура"
+    ws["C1"] = "Поточна погода"
+    ws["D1"] = "Мін. темп. (День 1)"
+    ws["E1"] = "Макс. темп. (День 1)"
+    ws["F1"] = "Мін. темп. (День 2)"
+    ws["G1"] = "Макс. темп. (День 2)"
+    ws["H1"] = "Мін. темп. (День 3)"
+    ws["I1"] = "Макс. темп. (День 3)"
+    ws["J1"] = "Мін.. темп. (День 4)"
+    ws["K1"] = "Макс. темп. (День 4)"
+    ws["L1"] = "Мін. темп. (День 5)"
+    ws["M1"] = "Макс. темп. (День 5)"
+
+    fill = PatternFill(start_color="ffd966", end_color="ffd966", 
+                       fill_type="solid")
+    font = Font(size=12, bold=True)
+    border = Border(left=Side(style="thin"), 
+                    right=Side(style="thin"), 
+                    top=Side(style="thin"), 
+                    bottom=Side(style="thin"))
+
+    for c in "ABCDEFGHIJKLM": 
+        cell = ws[f"{c}1"]
+        cell.fill = fill  
+        cell.font = font
+        cell.border = border
+
+        ws.column_dimensions[c].width = len(cell.value) + 10
 
 
 def update_xlsx(filename: str, city: str, data: list):
@@ -55,18 +95,7 @@ def update_xlsx(filename: str, city: str, data: list):
     else:
         ws = wb[city]
 
-    ws["A1"] = "Дата"
-    ws["B1"] = "Поточна температура"
-    ws["C1"] = "Мінімальна температура (День 1)"
-    ws["D1"] = "Максимальна температура (День 1)"
-    ws["E1"] = "Мінімальна температура (День 2)"
-    ws["H1"] = "Мінімальна температура (День 2)"
-    ws["I1"] = "Мінімальна температура (День 3)"
-    ws["J1"] = "Мінімальна температура (День 3)"
-    ws["K1"] = "Максимальна температура (День 4)"
-    ws["L1"] = "Максимальна температура (День 4)"
-    ws["M1"] = "Максимальна температура (День 5)"
-    ws["N1"] = "Максимальна температура (День 5)"
+    config_ws(ws)
 
     ws.append(data)
 
@@ -84,7 +113,7 @@ def format_city_name(city: str) -> str:
 
 def save_synoptic_stats(city: str, filename: str = "temp_stat_synoptik.xlsx"):
     cur_date = get_cur_date()   
-    data = parse_temp(city)
+    data = parse_data(city)
     city = format_city_name(city)
 
     update_xlsx(filename, city, [cur_date, *data])
