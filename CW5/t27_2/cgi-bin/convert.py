8a7e81a176b18fefa649caa1ce559f0c4f091f1f
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-03-12 09:49:34 +0200

    Completed task T27.2 with CGI

--- CW5/t27_2/cgi-bin/convert.py
+++ CW5/t27_2/cgi-bin/convert.py
@@ -1,16 +1,32 @@
-#!/home/x/Documents/Lectures/Prog2025/CW4/venv/bin/python
+#!/home/x/Documents/Lectures/Prog2025/CW5/t27_2/venv/bin/python
 
 import cgi
 import os
+import db
+from templates import get_template
+
+db.load_currency()
 
 CGI_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = CGI_DIR
 
 form = cgi.FieldStorage()
 
+def convert(amount: float, currency: str) -> float:
+    return amount * db.get_price(currency)
+
 amount = float(form.getfirst("amount", "0.00"))
-from_currency, to_currency = form.getfirst("currency", "USD/EUR").split("/")
+currency = form.getfirst("currency")
+new_amount = convert(amount, currency)
+
+from_currency, to_currency = currency.split("/")
+res = f"{amount:.2f} {from_currency} -> {new_amount:.2f} {to_currency}"
+
+index = get_template("index.html")
+html = index.render(currency=db.get_currency(),
+                    result=res)
+
 
 print("Content-Type: text/html")
 print()
-print(f"{amount} {from_currency} -> {to_currency}")
+print(html)
