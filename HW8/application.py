9ad9911d34d9dc5f61cf2a960cd2935c1d25bba6
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-04-08 12:07:26 +0300

    Fixed bug with headers

--- HW8/application.py
+++ HW8/application.py
@@ -5,6 +5,8 @@ from responce import HTMLResponce
 
 from urls import urlpatterns, compare_pattern
 
+from wsgiref.headers import Headers
+
 
 def application(environ, start_response):
     path = environ.get("PATH_INFO", "").lstrip("/")
@@ -25,13 +27,13 @@ def application(environ, start_response):
         parsed_data = urllib.parse.parse_qs(raw_post_data.decode("utf-8"))
         params = {key: value[0] for key, value in parsed_data.items()}
 
+    status, headers, body = HTMLResponce("404.html", 
+                                         status=StatusCode.S404)
     for pattern, view in urlpatterns:
         if compare_pattern(path, pattern):
             status, headers, body = view(path, params)
             break
-    else:
-        status, headers, body = HTMLResponce("404.html", 
-                                             status=StatusCode.S404)
 
+    headers = [*headers] # ? BUG AND HARDCODE
     start_response(status.value, headers)
     return [bytes(body, encoding="utf-8")]
