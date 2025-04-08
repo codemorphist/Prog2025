2a41d27c7293a0ad2b9ce4e56c5b1c919ab47b3b
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-04-08 12:55:01 +0300

    Added 500 handler

--- HW8/app/utils.py
+++ HW8/app/utils.py
@@ -5,6 +5,7 @@ class StatusCode(str, Enum):
     S200 = "200 OK"
     S400 = "400 BAD REQUEST"
     S404 = "404 NOT FOUND"
+    S500 = "500 INTERNAL SERVER ERROR"
 
 
 contenttype_html = ("Content-Type", "text/html; charset=utf-8")
--- HW8/app/wsgi.py
+++ HW8/app/wsgi.py
@@ -5,13 +5,13 @@ from app.responce import HTMLResponce
 
 from app.urls import urlpatterns, compare_pattern
 
+import traceback
+
 
 def application(environ, start_response):
+    # Get request path
     path = environ.get("PATH_INFO", "").lstrip("/")
     
-    query_string = environ.get("QUERY_STRING", "")
-    params = urllib.parse.parse_qs(query_string)
-
     length = environ.get('CONTENT_LENGTH', '0')
     try:
         length = int(length)
@@ -20,18 +20,28 @@ def application(environ, start_response):
 
     raw_post_data = environ["wsgi.input"].read(length) if length > 1 else b""
 
+    # Parse params form from (if exist)
     params = {}
     if raw_post_data:
         parsed_data = urllib.parse.parse_qs(raw_post_data.decode("utf-8"))
         params = {key: value[0] for key, value in parsed_data.items()}
 
+    # Default responce 404 HTTP Error
     status, headers, body = HTMLResponce("404.html", 
                                          status=StatusCode.S404)
+
     for pattern, view in urlpatterns:
         if compare_pattern(path, pattern):
-            status, headers, body = view(path, params)
-            break
+            try: # If patter found generate responce
+                status, headers, body = view(path, params)
+            except Exception as e: # If Exception return 500 HTTP error
+                status, headers, body = HTMLResponce(
+                        "500.html", 
+                        status=StatusCode.S500,
+                        context={"error": traceback.format_exc()}
+                    )
+            finally:
+                break
 
-    # headers = [*headers] # ? BUG AND HARDCODE
     start_response(status, headers)
     return [bytes(body, encoding="utf-8")]
