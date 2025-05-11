f121e5e6a2d8cfc3cc647ed73e77882992c47372
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2025-05-11 15:41:41 +0300

    Implemented program to write to system journal

---
+++ HW21/system_journal.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import random
+import time
+import os
+
+from config import JOURNAL_DIR
+
+
+
+def write_log():
+    timestamp = datetime.now().strftime("%X-%d-%m-%Y%z")
+    log_file = os.path.join(JOURNAL_DIR, f"{timestamp}.txt")
+    with open(log_file, "w") as f:
+        for _ in range(random.randint(1, 10)):
+            f.write(f"[INFO] Some log :: {datetime.now().strftime('%s')}\n")
+    print(f"[+] NEW LOG: {log_file}")
+
+
+def main():
+    print("== SYSTEM JOURNAL OPENED ==")
+    while True:
+        try:
+            write_log()
+            time.sleep(1.3)
+        except KeyboardInterrupt:
+            print("== SYSTEM JOURNAL CLOSE ==")
+            return
+
+
+if __name__ == "__main__":
+    main()
